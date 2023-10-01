4b363820398e30528735c9a4937e1434abddb4b2
samir-yagublu <samir.yagublu.std@bhos.edu.az>
2023-10-01 19:38:47 +0400

    last

---
+++ src/main/java/calculator.java
@@ -0,0 +1,11 @@
+public class calculator {
+
+        static int add (int a, int b){
+            return a+b;
+        }
+        static int multiply (int a, int b){
+            return a*b;
+        }
+
+
+}
